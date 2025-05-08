0f8828d420909e609dbb234d801c0fbfb686a58d
PranavDabhi36 <24ce020@charusat.edu.in>
2025-05-08 13:22:55 +0530

    pra 8.3.cpp

--- pra 8.3.cpp
+++ pra 8.3.cpp
@@ -1,24 +1,54 @@
 #include <iostream>
-#include <set>
 
 using namespace std;
 
-int main() {
-    int n, num;
-    set<int> uniqueIDs;
+// Simple bubble sort
+void sortArray(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
+// Remove duplicates from sorted array
+int removeDuplicates(int arr[], int n) {
+    if (n == 0) return 0;
+    int j = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[j]) {
+            j++;
+            arr[j] = arr[i];
+        }
+    }
+
+    return j + 1; // new length
+}
+
+int main() {
+    int n;
     cout << "Enter number of transaction IDs: ";
     cin >> n;
 
-    cout << "Enter the transaction IDs:" << endl;
-    for (int i = 0; i < n; ++i) {
-        cin >> num;
-        uniqueIDs.insert(num);  // Automatically removes duplicates
+    int arr[1000]; // Assuming max 1000 IDs
+
+    cout << "Enter the transaction IDs:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
-    cout << "Unique transaction IDs in sorted order:" << endl;
-    for (set<int>::iterator it = uniqueIDs.begin(); it != uniqueIDs.end(); ++it) {
-        cout << *it << " ";
+    sortArray(arr, n);
+    int newSize = removeDuplicates(arr, n);
+
+    cout << "\nUnique transaction IDs in sorted order:\n";
+    for (int i = 0; i < newSize; i++) {
+        cout << arr[i] << " ";
     }
     cout << endl;
 
